423f9b0efe1fde5db31e67b3662967c1e87aa992
Jobz9999 <238158993+Jobz9999@users.noreply.github.com>
2026-02-26 12:59:08 +0900

    rails s動作確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+   # --- Windows用: Sprocketsキャッシュ無効化 ---
+   config.assets.configure do |env|
+    env.cache = ActiveSupport::Cache::NullStore.new
+  end
 end
