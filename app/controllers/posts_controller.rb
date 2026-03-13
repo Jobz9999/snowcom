f39f67952925283ecc8045cc07ff06bec5f3df37
Jobz9999 <238158993+Jobz9999@users.noreply.github.com>
2026-03-13 17:15:41 +0900

    未承認の投稿不可に修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_community
+  before_action :require_approved_member_for_posting!, only: [:create]
+
   def create # 投稿の作成
-    @community = Community.find(params[:community_id]) # 投稿を作成するコミュニティを取得(コミュニティの詳細ページにリダイレクトするために必要)
     @post = @community.posts.new(post_params)          # 投稿をコミュニティに関連づけて作成
     @post.user = current_user                          # 投稿の作成者を現在のユーザーに設定
     if @post.save
@@ -11,7 +14,6 @@ class PostsController < ApplicationController
   end
 
   def destroy # 投稿の削除
-    @community = Community.find(params[:community_id]) 
     @post = @community.posts.find(params[:id])
 
     if @post.user == current_user || @community.user == current_user
@@ -23,14 +25,12 @@ class PostsController < ApplicationController
   end
 
   def edit # 投稿の編集
-    @community = Community.find(params[:community_id])
     @post = @community.posts.find(params[:id])
 
     redirect_to community_path(@community) unless @post.user == current_user
   end
 
   def update # 投稿の更新
-    @community = Community.find(params[:community_id])
     @post = @community.posts.find(params[:id])
 
     if @post.user == current_user
@@ -42,6 +42,20 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def set_community
+    @community = Community.find(params[:community_id])
+  end
+
+  def require_approved_member_for_posting!
+    return unless @community.approval_required?
+    return if @community.user == current_user
+
+    membership = @community.memberships.find_by(user: current_user)
+    return if membership&.approved?
+
+    redirect_to @community, alert: '承認制コミュニティでは参加ユーザーのみ投稿できます'
+  end
 
   def post_params # 投稿の作成時に必要なパラメータ
     params.require(:post).permit(:content, :image)
