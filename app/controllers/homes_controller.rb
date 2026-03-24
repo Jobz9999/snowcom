9ff453a0e5ce80c8568f56420cf4e01d40d9a755
Jobz9999 <238158993+Jobz9999@users.noreply.github.com>
2026-03-24 18:50:23 +0900

    人気ランキング追加+背景修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,14 @@
 class HomesController < ApplicationController
   def index
+    @popular_communities = Community
+      .left_joins(:posts, :memberships)
+      .select(
+        "communities.*",
+        "COUNT(DISTINCT posts.id) AS posts_count",
+        "COUNT(DISTINCT memberships.id) AS members_count"
+      )
+      .group("communities.id")
+      .order(Arel.sql("posts_count DESC, members_count DESC, communities.created_at DESC"))
+      .limit(5)
   end
 end
